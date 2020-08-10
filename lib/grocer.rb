67fa9f59272b761cde8f44e2e82101c59284e98c
Ashab Ahmed <ashabahmed7@gmail.com>
2020-08-10 23:18:22 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,8 +1,13 @@
+require 'pry'
+
 def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
-
+  
+  collection.each do |key, value|
+    binding.pry
+  end
 end
 
 def consolidate_cart(cart)
