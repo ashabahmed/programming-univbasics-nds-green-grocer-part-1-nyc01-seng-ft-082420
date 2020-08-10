264ad740f179ae4b17223bab46b1f7d2e2a3ce76
Ashab Ahmed <ashabahmed7@gmail.com>
2020-08-10 23:23:45 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -6,7 +6,8 @@ def find_item_by_name_in_collection(name, collection)
   # Consult README for inputs and outputs
   
   collection.each do |key|
-    binding.pry
+    if key[:item] == name
+    end
   end
 end
 
